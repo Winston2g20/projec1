c1233ef4328a1f891c4fe5f4d39a27d52bf8c749
haylia <karnichel@gmail.com>
2023-11-04 10:56:13 +0000

    added some basic player script

--- projec1/Assets/PlayerScript.cs
+++ projec1/Assets/PlayerScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Rigidbody rb;
+    public float speed;
+    // public float jumpsleft; // if want to implement double jump
+    // public bool dashleft; // if want to implement dashing
+    // private int maxdownspeed = 5; // max downspeed for the ground pound
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        bool gravity = true // always down for now
     }
 }
